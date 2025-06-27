4be29f2e3ffd5964fbc1aa94ba25aac8cc8e094f
K <white.devil.94484@gmail.com>
2025-06-27 13:49:50 +0530

    Add files via upload

---
+++ firebaseConfg.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCQNpkj3EZtF6KNiFcwNnQU3Ez0f7Ocr4U",
+    authDomain: "admin-database-ca73d.firebaseapp.com",
+    projectId: "admin-database-ca73d",
+    storageBucket: "admin-database-ca73d.firebasestorage.app",
+    messagingSenderId: "796828178511",
+    appId: "1:796828178511:web:1695f31e505d391d38723a"
+}; 
+export default firebaseConfig;
